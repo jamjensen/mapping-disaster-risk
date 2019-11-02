9332a73d504efb950acc4e15e6a0238f6d44d1bb
tglazer <tglazer@internationaldevelopmentgroup.com>
2019-11-02 16:34:13 -0500

    Modified raster_brick file and extracted file names into separate .py file

---
+++ file_names.py
@@ -0,0 +1,15 @@
+'''
+Local file names
+'''
+
+fpath_tiff_j = '/Users/jamesjensen/Documents/harris/q1_20/UML/Project/stac/' \
+			   'colombia/borde_rural/borde_rural_ortho-cog.tif'
+fpath_geojson_j = '/Users/jamesjensen/Documents/harris/q1_20/UML/Project/' \
+				  'stac/colombia/borde_rural/train-borde_rural.geojson'
+fpath_tiff_t = '/Users/Tammy/Documents/_MSCAPP/Fall 2019/Unsupervised ML/' \
+			   'Final Project/stac/colombia/borde_rural/borde_rural_' \
+			   'ortho-cog.tif'
+fpath_geojson_t = '/Users/Tammy/Documents/_MSCAPP/Fall 2019/Unsupervised ML/' \
+				  'Final Project/stac/colombia/borde_rural/train-borde_rural' \
+				  '.geojson'
+crs = 'epsg:32618'
--- raster_brick.py
+++ raster_brick.py
@@ -1,45 +1,57 @@
+'''
+Reads a geojson and geotiff file, extracts relevant information for one
+randomly selected rooftop, and displays the single rooftop as an image.
+'''
+
 import json
 import random
-
 from matplotlib import pyplot
 import rasterio
 from pyproj import Proj
 from rasterio.mask import mask
 from rasterio.plot import show
+import file_names as file
 
-# if importing into iypthon you need to run the magic command so that
-# you can print out the roof: 
-    # %matplotlib osx
+# Run this magic command in ipython to print out roof: %matplotlib osx
 
-fpath_tiff = '/Users/jamesjensen/Documents/harris/q1_20/UML/Project/stac/colombia/borde_rural/borde_rural_ortho-cog.tif'
-fpath_geojson = '/Users/jamesjensen/Documents/harris/q1_20/UML/Project/stac/colombia/borde_rural/train-borde_rural.geojson'
-
-dataset = rasterio.open(fpath_tiff)
-proj = Proj(init='epsg:32618')
+dataset = rasterio.open(file.fpath_tiff_t)
+proj = Proj(init=file.crs)
 
 
 def load_geojson(file_path):
     '''
     Given path to geojson file returns the geojson, which is of type
-    dictionary and stores the building id, roof material and cooridinates
-    of the building
+    dictionary that stores the building id, roof material (for training
+    purposes), and cooridinates of the building.
+
+    Input:
+        file_path (str): local path to geojson file
+
+    Output:
+        geojson (dict): dictionary containing building id, roof material,
+                        and coordinates for all buildings in a single file
     '''
 
-    with open(file_path) as geojson:
-        geojson = json.load(geojson)
+    with open(file_path) as gj:
+        geojson = json.load(gj)
 
     return geojson
     
-
-def make_polgyons(geojson):
+def make_polygons(geojson):
     '''
-    Given geojson dictionary returns a list of buildings mapped by their coordinates,
-    where each building is a dictionary. The building id and and roof material are 
-    included in the building dictionary. 
+    Given a full geojson dictionary, extracts relevant information for each
+    building, including type, building id, roof material, and coordinates.
+    Returns a list of dictionaries, where each dictionary maps a building by
+    these features.
+
+    Input:
+        geojson (dict): full geojson dictionary
+
+    Output:
+        polygons (lst): a list of dictionaries (one per building)
     '''
 
     polygons = []
-    polygon = {}
 
     for feature in geojson['features']:
         polygon = {}
@@ -51,64 +63,87 @@ def make_polgyons(geojson):
 
     return polygons
 
-
 def transform_coordinates(coordinates_lists, transform):
-    """Transform the coordinates with the provided `affine_transform`
-    
-    :param coordinates_lists: list of lists of coordinates
-    :type coordinates_lists: list[list[int]]
-    :param transform: transformation to apply
-    :type: pyproj.Proj
-    """
+    '''
+    Given a list of lists of coordinates for a single building (extracted
+    from a geojson), executes an affine transformation on each coordinate pair
+    to output a new coordinate pair based on a provided coordinate reference
+    system (crs) from the corresponding geotiff file.
+
+    Input:
+        coordinates_list (lst of lsts): a list of lists of coordinate pairs
+        transform (class instance): an initialized Proj class instance that
+                                    performs cartographic transformations
+
+    Output:
+        transformed_coordinate_lists (lst of lsts): a transformed list of lists
+                                                    of coordinate pairs
+    '''
     
     transformed_coordinates_lists = []
+    
     for coordinates_list in coordinates_lists:
-        transformed_coordinates_list = []
+        inner_coord_list = []
         for coordinate in coordinates_list:
-            coordinate = tuple(coordinate)
-            transformed_coordinate = list(transform(coordinate[0], coordinate[1]))
-            transformed_coordinates_list.append(transformed_coordinate)
-        transformed_coordinates_lists.append(transformed_coordinates_list)
+            transformed_coordinate = list(transform(coordinate[0],
+                                                    coordinate[1]))
+            inner_coord_list.append(transformed_coordinate)
+        transformed_coordinates_lists.append(inner_coord_list)
     
     return transformed_coordinates_lists
 
-
-def get_rooftop_array_after_mask(polygon, projection, tiff_path):
+def get_rooftop_array_after_mask(tiff_path, polygon, projection):
     '''
-    returns:
-        out_image (numpy array): array of pixel values for the given polygon 
+    Given a dictionary containing transformed coordinates for one roof and a
+    initialized Proj class instance to perform cartographic transformations,
+    returns an array of arrays, each one providing a numeric value for each
+    pixel in the image for a specific band (in this case, 4 matrices for 4
+    bands).
+
+    Eg. Values from 0-255 correspond with the color scale.
+
+    Input:
+        tiff_path (str): path to the tif file
+        polygon (dict): a dictionary of features including coordinates for a
+                        single building
+        projection (class instance): an initialized Proj class instance that
+                                     performs cartographic transformations
+
+    Output:
+        out_imate (numpy array): a numpy of values per pixel for each
+                                 band (one per image layer)
     '''
 
     with rasterio.open(tiff_path) as tiff:
         out_image, out_transform = mask(tiff, [polygon], crop=True)
 
     return out_image
-
-
-
     
 def display_single_roof(out_image):
     '''
-    takes numpy array for a single roof and displays it using matplot
+    Given a numpy array for a single roof, displays the image using matplot.
+
+    Input:
+        out_image (array): array of array of pixel values (one per band)
     '''
 
     show(out_image)
         
 
-
 if __name__ == "__main__":
+    '''
+    Script that reads a geojson and geotiff file, extracts relevant information
+    for one randomly selected rooftop, and displays the single rooftop as a
+    complete image.
+    '''
 
-    # This script processes and displays a single rooftop as a complete image
-    # (all raster bands)
-
-    geojson = load_geojson(fpath_geojson)
-    polygons = make_polgyons(geojson)
+    geojson = load_geojson(file.fpath_geojson_t)
+    polygons = make_polygons(geojson)
 
     idx = random.randint(0,len(polygons))
     polygon = polygons[idx] # gets single rooftop at random
 
-
-    polygon['coordinates'] = transform_coordinates(polygon['coordinates'], proj)
-    out_image = get_rooftop_array_after_mask(polygon, proj, fpath_tiff)
-
+    polygon['coordinates'] = transform_coordinates(polygon['coordinates'],
+                                                   proj)
+    out_image = get_rooftop_array_after_mask(file.fpath_tiff_t, polygon, proj)
     display_single_roof(out_image)
